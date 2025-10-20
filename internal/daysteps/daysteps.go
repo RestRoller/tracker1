bb3e92ed841eff61aefe67de9785f11db2441acc
Sergey Mokhov <rerollurmind@gmail.com>
2025-10-20 16:59:08 +0400

    Update daysteps.go
    123123

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,5 +1,12 @@
 package daysteps
 
+import (
+    "fmt"
+    "strconv"
+    "strings"
+    "time"
+)
+
 type DaySteps struct {
     steps int
 }
@@ -15,4 +22,26 @@ func (ds *DaySteps) Add(input string) error {
 
 func (ds *DaySteps) Steps() int {
     return ds.steps
+}
+
+// Добавляем функцию parseTraining в daysteps
+func parseTraining(input string) (int, time.Duration, float64, error) {
+    parts := strings.Fields(input)
+    if len(parts) != 3 {
+        return 0, 0, 0, fmt.Errorf("неверный формат данных")
+    }
+
+    steps, err := strconv.Atoi(parts[0])
+    if err != nil || steps <= 0 {
+        return 0, 0, 0, fmt.Errorf("неверное количество шагов")
+    }
+
+    duration, err := time.ParseDuration(parts[1] + parts[2])
+    if err != nil || duration <= 0 {
+        return 0, 0, 0, fmt.Errorf("неверная продолжительность")
+    }
+
+    distance := float64(steps) * 0.00075
+
+    return steps, duration, distance, nil
 }
