4f2986b3f1557efb0ff3ae6b53238aaff7699d31
Sergey Mokhov <rerollurmind@gmail.com>
2025-10-20 16:56:38 +0400

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,68 +1,18 @@
 package daysteps
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-	"time"
-)
-
 type DaySteps struct {
-	first *DayStepsItem
-}
-
-type DayStepsItem struct {
-	steps int
-	next  *DayStepsItem
+    steps int
 }
 
 func (ds *DaySteps) Add(input string) error {
-	steps, _, _, err := parseTraining(input)
-	if err != nil {
-		return err
-	}
-	
-	newItem := &DayStepsItem{steps: steps}
-	if ds.first == nil {
-		ds.first = newItem
-	} else {
-		current := ds.first
-		for current.next != nil {
-			current = current.next
-		}
-		current.next = newItem
-	}
-	
-	return nil
+    steps, _, _, err := parseTraining(input)
+    if err != nil {
+        return err
+    }
+    ds.steps += steps
+    return nil
 }
 
 func (ds *DaySteps) Steps() int {
-	total := 0
-	current := ds.first
-	for current != nil {
-		total += current.steps
-		current = current.next
-	}
-	return total
-}
-
-func parseTraining(input string) (int, time.Duration, float64, error) {
-	parts := strings.Fields(input)
-	if len(parts) != 3 {
-		return 0, 0, 0, fmt.Errorf("неверный формат данных")
-	}
-
-	steps, err := strconv.Atoi(parts[0])
-	if err != nil || steps <= 0 {
-		return 0, 0, 0, fmt.Errorf("неверное количество шагов")
-	}
-
-	duration, err := time.ParseDuration(parts[1] + parts[2])
-	if err != nil || duration <= 0 {
-		return 0, 0, 0, fmt.Errorf("неверная продолжительность")
-	}
-
-	distance := float64(steps) * 0.00075
-
-	return steps, duration, distance, nil
+    return ds.steps
 }
